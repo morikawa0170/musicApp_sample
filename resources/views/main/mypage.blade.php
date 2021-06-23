15651b3d3da55c87d88aa98d6355ca4cc122035a
morikawa0170 <mori7replicant10@gmail.com>
2021-06-24 00:19:24 +0900

    Myページに登録ボタン（静的）を追加

--- resources/views/main/mypage.blade.php
+++ resources/views/main/mypage.blade.php
@@ -8,6 +8,9 @@
             th, td {
                 border: 1px solid black;
             }
+            .td {
+                height: 100px;
+            }
         </style>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     </head>
@@ -23,7 +26,7 @@
             var token="";
             var type="";
             function search() {
-                var userID ="{{ $user_data -> spotifyid }}";
+                var userID ="{{ $user_data -> spotifyid }}"; //現在ログインしているアカウントのspotifyid
                 var ajax = new XMLHttpRequest();
                 ajax.open("post", "https://accounts.spotify.com/api/token");
                 // サーバに対して解析方法を指定する
@@ -37,28 +40,37 @@
                     token = json["access_token"];
             
                     var ajax2 = new XMLHttpRequest();
-                    ajax2.open("get", "https://api.spotify.com/v1/users/"+ userID +"/playlists?offset=0&limit=10");
+                    ajax2.open("get", "https://api.spotify.com/v1/users/"+ userID +"/playlists");
                     ajax2.setRequestHeader( 'Authorization', 'Authorization: Bearer '+ token
                     );
                     ajax2.send();
                     ajax2.responseType = "json";
                     ajax2.addEventListener("load", function(){ // loadイベントを登録します。
                         var html="<table class='container'>"
-                        html+="<tr><th>タイトル</th><th>コメント</th></tr>"
+                        html+="<tr><th>タイトル</th><th>コメント</th><th style='text-align:center;'>登録</th></tr>"
                         var json2 = this.response;
+                        //
                         for (var i=0;i<json2.items.length;i++) {
-                            var name=json2.items[i].name;
-                            var url=json2.items[i].external_urls.spotify;
+                            var name=json2.items[i].name; //プレイリスト名
                             var img=null;
-                            var playlistId = json2.items[i].id;
-                             if (json2.items[i].images.length > 2) {
+                            var playlistId = json2.items[i].id;　//プレイリストID
+                            var owner = json2.items[i].owner.id; //プレイリスト作成者のID
+                            var description = json2.items[i].description;
+                            //自分の作成したプレイリストのみを表示
+                            if(userID == owner) {
+                                if (json2.items[i].images.length > 2) {
                                   img=json2.items[i].images[2].url;
-                             }
-                            html += "<tr><td><a href='/musicApp/public/chat/"+playlistId+"'>";
-                            if (img!=null) {
-                                html += "<img src='"+img+"'width=100px height=100px'>";
+                                 }
+                                html += "<tr><td class='td'><a href='/musicApp/public/chat/"+playlistId+"'>";
+                                //画像がある場合は表示
+                                if (img!=null) {
+                                    html += "<img src='"+img+"'width=100px height=100px'>";
+                                }
+                                var button = "<button type='button' class='btn btn-success'>登録</button>"
+                                html += name+"</a></td><td class='td'><pre>"+description+"</pre></td><td style='text-align:center;'>"+button+"</td></tr>";
+                            }else {
+                                continue;
                             }
-                            html += name+"</a></td><td><textarea name='comment'></textarea></td></tr>";
                         }
                         var list = document.getElementById("list");
                         list.innerHTML = html + "</table>";
