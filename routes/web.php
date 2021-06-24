c2598a28f9e2d4935a5cc9348850e8e4d9a6e6b9
morikawa0170 <mori7replicant10@gmail.com>
2021-06-24 22:45:56 +0900

    function名の変更

--- routes/web.php
+++ routes/web.php
@@ -22,16 +22,12 @@ Route::get('/login/', function (){
     return view('login.login');
 });
 
-Route::get('/mypage/{username}',[AppUserController::class,'edit']); //マイページを表示
-
-Route::get('/chat/{title}',[AppUserController::class,'show']); //チャット画面を表示
-
+Route::get('/mypage/{username}',[AppUserController::class,'mypage']); //マイページを表示
+Route::get('/chat/{title}',[AppUserController::class,'chat']); //チャット画面を表示
 Route::post('/login', [LoginController::class, 'login']); // ログインを実行
-
 Route::get('/logout', [LoginController::class,'logout']); //ログアウトを実行
-
-Route::post('/chatajax/{title}',[AppUserController::class,'store']); //チャットの処理
-Route::get('/chatajax/{title}',[AppUserController::class,'create']); //チャットを表示
+Route::post('/chatajax/{title}',[AppUserController::class,'chatStore']); //チャットの処理
+Route::get('/chatajax/{title}',[AppUserController::class,'chatCreate']); //チャットを表示
 
 
 Route::get('/register/', function (){ //新規登録
