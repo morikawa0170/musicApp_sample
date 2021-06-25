b98fd2f972b109dac746770f08a1ff3f03ac865e
morikawa0170 <mori7replicant10@gmail.com>
2021-06-26 08:32:10 +0900

    playlist model,controllerの作成

---
+++ app/Http/Controllers/PlaylistController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Playlist;
+
+class PlaylistController extends Controller
+{
+   public function create(Request $request) //プレイリスト情報登録機能
+   {
+      
+   }
+}
